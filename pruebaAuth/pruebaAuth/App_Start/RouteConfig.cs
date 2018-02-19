89e8ecbdf41d8f0e10e2fd73fee0a89eaef6f704
j21david <jose21gonzalez@gmail.com>
2018-02-19 09:36:56 -0500

    Final-Commit

--- pruebaAuth/pruebaAuth/App_Start/RouteConfig.cs
+++ pruebaAuth/pruebaAuth/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace pruebaAuth
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+				defaults: new { controller = "Reserva", action = "Index", id = UrlParameter.Optional }
 			);
 		}
 	}
